a13dead7f41ca62997e1617cae48de98228dc5d5
kdayns <kdayns@gmail.com>
2014-12-22 12:34:33 +0200

    temp thread, discovery thread

--- temp.py
+++ temp.py
@@ -2,32 +2,89 @@
 
 import sys
 import json
+import time
 import http.server
 import socketserver
-from os import curdir, sep
+from os import curdir, sep, listdir
 import datetime;
 import subprocess;
 import threading;
 
-debug   = 1
-root    = 'web'
-PORT_NUMBER     = 8080
+# config
+updateInterval  = 6
+debug           = 1
 
+datadir         = 'data'
+PORT_NUMBER     = 8080
 config = {
-    'sensors': { 'xxxx': ['name', 1, 2]},
-    'brewfiles': ["test"],
-    'active': 'test',
+    'sensors'   : { 'xxxx': ['name', 1, 2]},
+    'brewfiles' : [],
+    'active'    : 'test',
+    'running'   : False,
 }
 
-def update_temp():
+# private
+#w1path = '/sys/bus/w1/devices/'
+w1path = '/sys/bus/usb/devices/'
+name = 0
+curr = 1
+avg = 2
+
+subprocess.call(['modprobe', 'w1-gpio', 'gpiopin=10'])
+subprocess.call(['modprobe', 'w1_therm'])
+
+def thread_update_temp(d):
+    # TODO - read sensor
     return
 
+def thread_temp():
+    global config
+    sensors = config['sensors']
 
-#while True:
-#    update_temp()
-#sleep(updateInterval)
+    while True:
+        # TODO - lock sensors
+        for k,d in sensors.items(): d[avg] = 0
 
+        rthreads = []
+        for k,d in sensors.items():
+            th = threading.Thread(daemon=True, target=thread_update_temp, args=(d,))
+            th.start()
+            rthreads.append(th)
+        for th in rthreads:
+            th.join()
+        if debug: print('waiting: ', updateInterval)
+        threading.Event().wait(timeout=updateInterval)
 
+def thread_shutdown():
+    while True:
+        # TODO - monitor gpio
+        # /sys/class/gpio
+        threading.Event().wait(timeout=1)
+    return
+
+def thread_discovery():
+    global config
+    sensors = config['sensors']
+    brews = config['brewfiles']
+
+    while True:
+        # monitor w1
+        found = False
+        for f in listdir(w1path):
+            if f == 'w1_bus_master1': continue
+
+            if not f in sensors.items():
+                print("new sensor: " + f)
+                found = True
+                sensors['f'] = [ f, 0, 0]
+        if found: open('config', 'w').write(json.dumps(config))
+
+        # monitor data dir
+        for f in listdir('data'):
+            if not f in brews: brews.append(f)
+
+        threading.Event().wait(timeout=5)
+    return
 
 class myHandler(http.server.BaseHTTPRequestHandler):
     def handle_one_request(self):
@@ -77,6 +134,10 @@ class myHandler(http.server.BaseHTTPRequestHandler):
             self.send_error(404,'File Not Found: %s' % self.path)
         except:
             self.send_error(500,'Unknown error: %s' % sys.exc_info()[0])
+
+threading.Thread(daemon=True, target=thread_temp).start()
+threading.Thread(daemon=True, target=thread_shutdown).start()
+threading.Thread(daemon=True, target=thread_discovery).start()
 
 try:
     server = http.server.HTTPServer(('', PORT_NUMBER), myHandler)
